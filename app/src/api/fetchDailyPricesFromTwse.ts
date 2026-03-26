b501f8b22e074b143f05c7366497fb260bdd046f
ChocoMilktea <milktea126421@gmail.com>
2026-03-26 01:47:46 +0000

    feat(dailyPrice): add fetchDailyPricesFromTwse function and test with vitest

---
+++ app/src/api/fetchDailyPricesFromTwse.ts
@@ -0,0 +1,18 @@
+export async function fetchDailyPricesFromTwse(
+  year: string,
+  month: string,
+  day: string,
+): Promise<void> {
+  const url = `https://www.twse.com.tw/rwd/zh/afterTrading/MI_INDEX?date=${year}${month}${day}&type=ALLBUT0999&response=json`;
+
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`HTTP error! ${response.status} ${response.statusText}`);
+  }
+  const data = await response.json();
+  if (data.stat !== "OK") {
+    throw new Error(`API error! ${data.stat}`);
+  }
+
+  return data;
+}
---
+++ app/src/api/fetchDailypricesFromTwse.test.ts
@@ -0,0 +1,63 @@
+import { describe, expect, it, vi } from "vitest";
+import { fetchDailyPricesFromTwse } from "./fetchDailyPricesFromTwse";
+
+describe("fetchDailypricesFromTwse", () => {
+  it("should fetch data", async () => {
+    const mockData = {
+      stat: "OK",
+      date: "20240101",
+      tables: [
+        {
+          title: "mock title",
+          fields: [
+            "field1",
+            "field2",
+          ],
+          data: [
+            [
+              "value1",
+              "value2",
+            ],
+          ],
+        },
+      ],
+    };
+
+    globalThis.fetch = vi.fn().mockResolvedValue({
+      ok: true,
+      json: async () => mockData,
+    });
+
+    const result = await fetchDailyPricesFromTwse("2024", "01", "01");
+
+    expect(globalThis.fetch).toHaveBeenCalledExactlyOnceWith(`https://www.twse.com.tw/rwd/zh/afterTrading/MI_INDEX?date=20240101&type=ALLBUT0999&response=json`);
+    expect(result).toEqual(mockData);
+  });
+
+  it("should throw if response is not ok", async () => {
+    globalThis.fetch = vi.fn().mockResolvedValue({
+      ok: false,
+      status: 500,
+      statusText: "Internal Server Error",
+    });
+
+    await expect(fetchDailyPricesFromTwse("2024", "01", "01")).rejects.toThrow(
+      "HTTP error! 500 Internal Server Error"
+    );
+  });
+
+  it("should throw if data.stat is not 'OK'", async () => {
+    const mockData = {
+      stat: "ERROR",
+    };
+
+    globalThis.fetch = vi.fn().mockResolvedValue({
+      ok: true,
+      json: async () => mockData,
+    });
+
+    await expect(fetchDailyPricesFromTwse("2024", "01", "01")).rejects.toThrow(
+      "API error! ERROR"
+    );
+  });
+});
