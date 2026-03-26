e8a9a1c29ff43f7fdc0767fc29a710cd155129c0
ChocoMilktea <milktea126421@gmail.com>
2026-03-26 07:28:55 +0000

    refactor(api): update fetchDailyPricesFromTwse return type and export method

--- app/src/api/fetchDailyPricesFromTwse.ts
+++ app/src/api/fetchDailyPricesFromTwse.ts
@@ -1,6 +1,6 @@
-export async function fetchDailyPricesFromTwse(
+async function fetchDailyPricesFromTwse(
   date: string,
-): Promise<any> {
+): Promise<unknown> {
   const url = `https://www.twse.com.tw/rwd/zh/afterTrading/MI_INDEX?date=${date}&type=ALLBUT0999&response=json`;
 
   const response = await fetch(url);
@@ -14,3 +14,5 @@ export async function fetchDailyPricesFromTwse(
 
   return data;
 }
+
+export default fetchDailyPricesFromTwse;
--- app/src/api/fetchDailypricesFromTwse.test.ts
+++ app/src/api/fetchDailypricesFromTwse.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it, vi } from "vitest";
-import { fetchDailyPricesFromTwse } from "./fetchDailyPricesFromTwse";
+import fetchDailyPricesFromTwse from "./fetchDailyPricesFromTwse";
 
 describe("fetchDailypricesFromTwse", () => {
   it("should fetch data", async () => {
