f70ef010db433d04fe00727ef83fc1947300a4e3
ChocoMilktea <milktea126421@gmail.com>
2026-03-27 09:26:53 +0000

    chore(vitest): update vite.config.ts to optimize and monitor test setup

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -16,12 +16,33 @@ export default defineConfig({
         target: "https://www.tpex.org.tw",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/tpexApi/, ""),
-      }
-    }
+      },
+    },
   },
   test: {
     environment: "jsdom",
     setupFiles: "./src/setupTest.ts",
     globals: true,
+    include: ["src/**/*.test.{ts,tsx}"],
+    deps: {
+      optimizer: {
+        client: {
+          enabled: true,
+          include: [
+            "@emotion/react",
+            "@emotion/styled",
+            "@mui/material",
+            "@mui/x-date-pickers",
+            "dayjs",
+          ],
+        },
+      },
+    },
+    experimental: {
+      importDurations: {
+        print: true,
+        thresholds: { warn: 50 },
+      },
+    },
   },
 });
