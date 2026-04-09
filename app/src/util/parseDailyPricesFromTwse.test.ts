b17df56e38423129d9cde3f65215adc839b6312d
milktea <milktea126421@gmail.com>
2026-04-09 14:50:18 +0000

    feat(util): update parseDailyPricesFromTwse to remove comma in data

--- app/src/util/parseDailyPricesFromTwse.test.ts
+++ app/src/util/parseDailyPricesFromTwse.test.ts
@@ -4,7 +4,7 @@ import parseDailyPricesFromTwse, { DailyPriceTargetFields } from "./parseDailyPr
 describe("parseDailyPricesFromTwse", () => {
   it("should parse the target table into array of objects with target fields", () => {
     const mockValue = DailyPriceTargetFields.reduce((acc, _field, index) => {
-      const value = index < 2 ? `<p>value${index + 1}</p>` : `value${index + 1}`;
+      const value = index < 2 ? `<p>value${index + 1},000.0</p>` : `value${index + 1}`;
       acc.push(value);
       return acc;
     }, [] as string[]);
@@ -23,7 +23,7 @@ describe("parseDailyPricesFromTwse", () => {
     const result = parseDailyPricesFromTwse(mockData, "mock title");
 
     const expectedResult = DailyPriceTargetFields.reduce((acc, field, index) => {
-      acc[field] = `value${index + 1}`;
+      acc[field] = index < 2 ? `value${index + 1}000.0` : `value${index + 1}`;
       return acc;
     }, {} as Record<string, string>);
     expect(result).toEqual([expectedResult]);
--- app/src/util/parseDailyPricesFromTwse.ts
+++ app/src/util/parseDailyPricesFromTwse.ts
@@ -51,7 +51,7 @@ function getTargetTable(
 }
 
 function normalizeCell(value: string | undefined): string {
-  return value ? value.replace(/<[^>]*>/g, '').trim() : "";
+  return value ? value.replace(/<[^>]*>/g, "").replace(/,/g, "").trim() : "";
 }
 
 function parseTableData(table: {
